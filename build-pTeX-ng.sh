0eeb9cc7e0d4d1b1962d6a2d4c786b7e8c9a42ca
Liam Huang <liamhuang0205@gmail.com>
2014-12-09 16:45:07 +0800

    add building shell script
    building chain:
    * zlib
    * libpng
    * kpathsea
    * ptexenc
    * libdpx
    * ptex-ng

---
+++ build-pTeX-ng.sh
@@ -0,0 +1,12 @@
+export NG_SRC_DIR=`pwd`
+cd $NG_SRC_DIR/libs/zlib && ./configure && make
+cp libz.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/libs/libpng && ./configure && make
+cp libpng.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/texk/kpathsea && ./configure && make
+cp .libs/libkpathsea.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/texk/ptexenc && ./configure && make
+cp .libs/libptexenc.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/texk/libdpx && ./configure && make
+cp libdpx.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/texk/ptex-ng && make
