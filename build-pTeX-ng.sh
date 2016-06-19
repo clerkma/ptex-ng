d185c2e4236cd5b625c963088973d38bf0cfb279
clerkma <clerkma@gmail.com>
2016-06-20 00:13:52 +0800

    updated build scripts.

--- build-pTeX-ng.sh
+++ build-pTeX-ng.sh
@@ -5,10 +5,14 @@ cd $NG_SRC_DIR/libs/libpng && ./configure && make
 cp libpng.a $NG_SRC_DIR/texk/ptex-ng
 cd $NG_SRC_DIR/libs/libpaper && ./configure && make
 cp libpaper.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/libs/freetype2 && ./configure && make
+cp libfreetype.a $NG_SRC_DIR/texk/ptex-ng
 cd $NG_SRC_DIR/texk/kpathsea && ./configure && make
 cp .libs/libkpathsea.a $NG_SRC_DIR/texk/ptex-ng
 cd $NG_SRC_DIR/texk/ptexenc && ./configure && make
 cp .libs/libptexenc.a $NG_SRC_DIR/texk/ptex-ng
 cd $NG_SRC_DIR/texk/libdpx && ./configure && make
 cp libdpx.a $NG_SRC_DIR/texk/ptex-ng
+cd $NG_SRC_DIR/texk/ptex-ng/libotf && ./configure && make
+cp src/.libs/libotf.a $NG_SRC_DIR/texk/ptex-ng
 cd $NG_SRC_DIR/texk/ptex-ng && make
