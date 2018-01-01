99c6a1ef0af9a9dca1ce4472f5dc58b35b92d48d
Clerk Ma <clerkma@gmail.com>
2018-01-02 01:11:51 +0800

    and updated ignore files.

--- build-aptex.sh
+++ build-aptex.sh
@@ -2,26 +2,26 @@
 
 export NG_SRC_DIR=`pwd`
 # libraries from TeX Live.
-cd $NG_SRC_DIR/texlive/libs/zlib && ./configure && make
+cd $NG_SRC_DIR/texlive/libs/zlib && ./configure && make -j
 cp libz.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/texlive/libs/libpng && ./configure && make
+cd $NG_SRC_DIR/texlive/libs/libpng && ./configure && make -j
 cp libpng.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/texlive/libs/libpaper && ./configure && make
+cd $NG_SRC_DIR/texlive/libs/libpaper && ./configure && make -j
 cp libpaper.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/texlive/libs/freetype2 && ./configure && make
+cd $NG_SRC_DIR/texlive/libs/freetype2 && ./configure && make -j
 cp libfreetype.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/texlive/libs/pixman && ./configure && make
+cd $NG_SRC_DIR/texlive/libs/pixman && ./configure && make -j
 cp libpixman.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/texlive/texk/kpathsea && ./configure && make
+cd $NG_SRC_DIR/texlive/texk/kpathsea && ./configure && make -j
 cp .libs/libkpathsea.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/texlive/texk/ptexenc && ./configure && make
+cd $NG_SRC_DIR/texlive/texk/ptexenc && ./configure && make -j
 cp .libs/libptexenc.a $NG_SRC_DIR/src
 # 
-cd $NG_SRC_DIR/src/libcairo && make
+cd $NG_SRC_DIR/src/libcairo && make -j
 cp libcairo.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/src/libdpx && make
+cd $NG_SRC_DIR/src/libdpx && make -j
 cp libdpx.a $NG_SRC_DIR/src
-cd $NG_SRC_DIR/src/libotf && make
+cd $NG_SRC_DIR/src/libotf && make -j
 cp libotf.a $NG_SRC_DIR/src
 #
 cd $NG_SRC_DIR/src/libyaml
@@ -36,7 +36,7 @@ gcc -I include -I win32 -DHAVE_CONFIG_H -DYAML_DECLARE_STATIC -c src/writer.c
 ar rcu libyaml.a api.o dumper.o emitter.o loader.o parser.o reader.o scanner.o writer.o
 cp libyaml.a $NG_SRC_DIR/src
 #
-cd $NG_SRC_DIR/src/mruby && make
+cd $NG_SRC_DIR/src/mruby && make -j
 cp build/host/lib/libmruby.a $NG_SRC_DIR/src
 #
-cd $NG_SRC_DIR/src && make
+cd $NG_SRC_DIR/src && make -j
