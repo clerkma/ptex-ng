e798c2d054537321a99a10a95742dab4a327bc29
Clerk Ma <clerkma@gmail.com>
2018-01-31 14:15:39 +0800

    synchronized texlive 46499.

--- texlive/texk/texlive/linked_scripts/lwarp/lwarpmk.lua
+++ texlive/texk/texlive/linked_scripts/lwarp/lwarpmk.lua
@@ -4,7 +4,7 @@
 
 -- Print the usage of the lwarpmk command:
 
-printversion = "v0.46"
+printversion = "v0.47"
 
 function printhelp ()
 print ("lwarpmk: Use lwarpmk -h or lwarpmk --help for help.") ;
@@ -280,7 +280,7 @@ err = os.execute(
  lwimgpage .. " " .. sourcename .."_html.pdf lateximagetemp-%d.pdf")
 -- Crop the image:
 err = os.execute(
-"pdfcrop --hires lateximagetemp-" .. lwimgpage ..".pdf lateximage-" .. lwimgnum ..".pdf")
+"pdfcrop  lateximagetemp-" .. lwimgpage ..".pdf lateximage-" .. lwimgnum ..".pdf")
 if ( err ~= 0 ) then print ( "lwarpmk: File error.") ; os.exit(1) ; end
 -- Convert the image to svg:
 err = os.execute(
