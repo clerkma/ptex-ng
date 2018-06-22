1f1f281c2a97f1359467974cc2764095bce858c5
Clerk Ma <clerkma@gmail.com>
2018-06-23 02:24:13 +0800

    fixed a typo and added clean-aptex.sh.

---
+++ clean-aptex.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export NG_SRC_DIR=`pwd`
+cd $NG_SRC_DIR/texlive/libs/zlib && make distclean
+cd $NG_SRC_DIR/texlive/libs/libpng && make distclean
+cd $NG_SRC_DIR/texlive/libs/libpaper && make distclean
+cd $NG_SRC_DIR/texlive/libs/freetype2 && make distclean
+cd $NG_SRC_DIR/texlive/libs/pixman && make distclean
+cd $NG_SRC_DIR/texlive/texk/kpathsea && make distclean
+cd $NG_SRC_DIR/texlive/texk/ptexenc && make distclean
+cd $NG_SRC_DIR/src/libcairo && rm -f *.o *.a
+cd $NG_SRC_DIR/src/libdpx && rm -f *.o *.a
+cd $NG_SRC_DIR/src/libotf && rm -f *.o *.a
+cd $NG_SRC_DIR/src/libyaml && rm -f *.o *.a
+cd $NG_SRC_DIR/src/mruby && make clean
+cd $NG_SRC_DIR/src && rm -f *.o aptex ptex-ng
