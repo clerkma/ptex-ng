8d69b4e78ba30b71f432bd380dd76129c9e9b0d7
Clerk Ma <clerkma@gmail.com>
2018-09-22 17:51:32 +0800

    synchronized texlive 48731.

--- texlive/texk/texlive/linked_scripts/l3build/l3build.lua
+++ texlive/texk/texlive/linked_scripts/l3build/l3build.lua
@@ -25,7 +25,7 @@ for those people who are interested.
 --]]
 
 -- Version information
-release_date = "2018-08-07"
+release_date = "2018-09-21"
 
 -- File operations are aided by the LuaFileSystem module
 local lfs = require("lfs")
