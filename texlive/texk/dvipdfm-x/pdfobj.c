60be95bcc2440a8c5a6cc0ad15c2ff47710943c0
Clerk Ma <clerkma@gmail.com>
2018-12-27 15:37:00 +0800

    synchronized texlive 49509.

--- texlive/texk/dvipdfm-x/pdfobj.c
+++ texlive/texk/dvipdfm-x/pdfobj.c
@@ -476,10 +476,11 @@ dump_xref_stream (void)
 }
 
 #if defined(LIBDPX)
+static long pdf_output_file_stats = 0;
 long
 pdf_output_stats (void)
 {
-  return pdf_output_file_position;
+  return pdf_output_file_stats;
 }
 #endif /* LIBDPX */
 
@@ -535,6 +536,8 @@ pdf_out_flush (void)
     }
 #if !defined(LIBDPX)
     MESG("%ld bytes written", pdf_output_file_position);
+#else
+    pdf_output_file_stats = pdf_output_file_position;
 #endif /* !LIBDPX */
 
     MFCLOSE(pdf_output_file);
