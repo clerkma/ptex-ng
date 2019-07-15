4599ebac517d2afe37c6f044d55637fa62cb9fde
Clerk Ma <clerkma@gmail.com>
2019-07-16 01:49:51 +0800

    synchronized texlive 51644.

--- texlive/texk/texlive/linked_scripts/luaotfload/luaotfload-tool.lua
+++ texlive/texk/texlive/linked_scripts/luaotfload/luaotfload-tool.lua
@@ -9,8 +9,8 @@
 
 local ProvidesLuaModule = { 
     name          = "luaotfload-tool",
-    version       = "2.97",       --TAGVERSION
-    date          = "2019-05-18", --TAGDATE
+    version       = "2.98",       --TAGVERSION
+    date          = "2019-07-04", --TAGDATE
     description   = "luaotfload-tool / database functionality",
     license       = "GPL v2.0"
 }
