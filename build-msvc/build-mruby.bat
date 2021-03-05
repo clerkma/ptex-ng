56428c0441648dfdbb233c0c47c006dcb4890fc5
Clerk Ma <clerkma@gmail.com>
2021-03-05 17:49:07 +0800

    fixed mruby (msvc build).

--- build-msvc/build-mruby.bat
+++ build-msvc/build-mruby.bat
@@ -2,7 +2,7 @@ if exist mruby (
   rd /s /q mruby
 )
 xcopy ..\src\mruby mruby\ /e/h
-set MRUBY_CONFIG=appveyor_config.rb
+set MRUBY_CONFIG=ci/msvc
 set CFLAGS=-nologo -c -O2 -Oy
 cd mruby
 ruby .\minirake all
