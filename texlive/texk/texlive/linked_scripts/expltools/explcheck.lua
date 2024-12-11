cad28f76a1985af40b0e32f7624df6a7bdfb029d
Clerk Ma <clerkma@gmail.com>
2024-12-11 13:32:24 +0800

    synchronized texlive 73098.

--- texlive/texk/texlive/linked_scripts/expltools/explcheck.lua
+++ texlive/texk/texlive/linked_scripts/expltools/explcheck.lua
@@ -1,3 +1,7 @@
 #!/usr/bin/env texlua
 -- A command-line interface for the static analyzer explcheck.
+local using_luatex, kpse = pcall(require, "kpse")
+if using_luatex then
+  kpse.set_program_name("texlua", "explcheck")
+end
 require("explcheck-cli")
