b58b2109c7048ae9e91b5f5e677a8bd253123594
Clerk Ma <clerkma@gmail.com>
2024-12-23 11:31:14 +0800

    synchronized texlive 73191.

--- texlive/texk/web2c/luatexdir/luatex.c
+++ texlive/texk/web2c/luatexdir/luatex.c
@@ -33,8 +33,8 @@
 */
 
 int luatex_version = 119;
-int luatex_revision = '9';
-const char *luatex_version_string = "1.19.0";
+int luatex_revision = '1';
+const char *luatex_version_string = "1.19.1";
 const char *engine_name = my_name;
 
 #include <kpathsea/c-ctype.h>
