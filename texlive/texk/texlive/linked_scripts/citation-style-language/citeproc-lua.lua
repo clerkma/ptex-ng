ad2716cbe52fd8757fc101c3e0e83f90a5b5ae33
Clerk Ma <clerkma@gmail.com>
2025-01-11 12:24:15 +0800

    synchronized texlive 73406.

--- texlive/texk/texlive/linked_scripts/citation-style-language/citeproc-lua.lua
+++ texlive/texk/texlive/linked_scripts/citation-style-language/citeproc-lua.lua
@@ -1,10 +1,15 @@
 #!/usr/bin/env texlua
 
 --
--- Copyright (c) 2021-2024 Zeping Lee
+-- Copyright (c) 2021-2025 Zeping Lee
 -- Released under the MIT license.
 -- Repository: https://github.com/zepinglee/citeproc-lua
 --
+
+local using_luatex, kpse = pcall(require, "kpse")
+if not using_luatex then
+  error("citeproc-lua must be run with LuaTeX")
+end
 
 kpse.set_program_name("texlua", "citeproc-lua")
 
