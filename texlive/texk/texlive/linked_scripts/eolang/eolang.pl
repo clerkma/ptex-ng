164f4f40e3ccf0d93d3df4976578ee61136b73b7
Clerk Ma <clerkma@gmail.com>
2025-03-27 11:57:14 +0800

    synchronized texlive 74763.

--- texlive/texk/texlive/linked_scripts/eolang/eolang.pl
+++ texlive/texk/texlive/linked_scripts/eolang/eolang.pl
@@ -1,37 +1,52 @@
 #!/usr/bin/perl
-# (The MIT License)
-#
-# Copyright (c) 2021-2024 Yegor Bugayenko
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the 'Software'), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
 
-# 0000-00-00 0.18.2
+# 2025/03/26 0.18.3
 package eolang;
 
 use warnings;
 use strict;
-use lib('.');
-use tools;
 use File::Basename;
 
 # Hash of incoming command line arguments.
 my %args = map { $_ => 1 } @ARGV;
+
+# Read file content.
+sub readfile {
+  my ($path) = @_;
+  open(my $h, '<', $path) or die('Cannot read from file: ' . $path);
+  my $content; { local $/; $content = <$h>; }
+  return $content;
+}
+
+# Save content to file.
+sub savefile {
+  my ($path, $content) = @_;
+  open(my $f, '>', $path) or error('Cannot open file for writing: ' . $path);
+  print $f $content;
+  close($f);
+}
+
+# Print INFO message to the console.
+sub info {
+  my ($txt) = @_;
+  print $txt . "\n";
+}
+
+# Print DEBUG message to the console.
+sub debug {
+  my ($txt) = @_;
+  if (exists $args{'--verbose'}) {
+    print $txt . "\n";
+  }
+}
+
+# Print ERROR message to the console.
+sub error {
+  my ($txt) = @_;
+  print STDERR $txt . "\n";
+}
 
 if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
   info("This script helps embedding \\phiquation and \\phiq into .tex document\n\n" .
@@ -44,7 +59,7 @@ if (@ARGV+0 eq 0 or exists $args{'--help'} or exists $args{'-?'}) {
     "      --tmpdir=path    Temp directory with .tex files ('_eolang' by default)\n\n" .
     "If any issues, report to GitHub: https://github.com/yegor256/bibcop");
 } elsif (exists $args{'--version'} or exists $args{'-v'}) {
-  info('0.18.2 0000-00-00');
+  info('0.18.3 0000-00-00');
 } else {
   my ($src, $target) = grep { not($_ =~ /^-.*$/) } @ARGV;
   if (not $src) {
