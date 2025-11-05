6e47b56dae67e8c58a95504815e482f688769a5e
Clerk Ma <clerkma@gmail.com>
2025-11-05 22:23:26 +0800

    added msvc env scripts.

---
+++ build-msvc/env/set-env-x86.bat
@@ -0,0 +1,16 @@
+@echo off
+
+for /f "usebackq tokens=*" %%i in (
+`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"^
+ -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.*^
+ -property installationPath`) do (
+  set InstallDir=%%i
+)
+set VSENVBAT="%InstallDir%\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+if exist %VSENVBAT% (
+    call %VSENVBAT%
+    exit /b 1
+)
+
+echo "I cannot found compiler. Please make sure that you have installed it."
+@echo on
