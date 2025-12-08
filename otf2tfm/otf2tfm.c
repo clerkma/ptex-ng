272f52fc88db4d007b76298f054c713c368bb60f
LdBeth <andpuke@foxmail.com>
2025-12-08 17:21:25 +0800

    squish

--- otf2tfm/otf2tfm.c
+++ otf2tfm/otf2tfm.c
@@ -9,31 +9,23 @@ typedef int I;typedef unsigned int UI;typedef char* S;typedef FT_Face F;
 #define g f->glyph
 #define p printf
 #define R return
+#define _I static I
 #define UK FT_Err_Unknown_File_Format
 #define M(b...) for(n=0;n<256;++n){b;}
-#define _ static void
-#define FN(n) _ n(F f)
+#define FN(n) static void n(F f)
 #define inpp /(double)ppem
-F f;static FT_Error err;I wd[256];I ht[256];I dp[256];
-static I cd(I *a){I c=0;I n;
+static F f;static FT_Error err;static FT_BBox b;I wd[256];I ht[256];I dp[256];
+_I cd(I *a){I c=0;I n;
   M(I j;for(j=n+1;j<256;j++)if(a[n]==a[j])break;if(j==256)c++;);
   R c;}
-_ bbox(FT_Outline* o,int *h, int* d){FT_BBox b;
-  FT_Outline_Get_BBox(o,&b);*h=b.yMax;*d=b.yMin;}
 FN(met){UI n;
-  M(err=FT_Load_Glyph(f,n,FT_LOAD_NO_SCALE);
-    b(err,p("load glyph %d failed\n",n),);
-    wd[n]=g->advance.x;
-    bbox(&g->outline,&ht[n],&dp[n]););}
-I main(I ac,S av[]){FT_Library l;
-  FT_Init_FreeType(&l);
-  err=FT_New_Face(l,FONT,0,&f);
-  b(err==UK,p("fmt err: %s\n",FONT),b(err,p("other err: %d",err),));
+  M(b(FT_Load_Glyph(f,n,FT_LOAD_NO_SCALE),p("load glyph %d failed\n",n),);
+    wd[n]=g->advance.x;FT_Outline_Get_BBox(&g->outline,&b);ht[n]=b.yMax,dp[n]=b.yMin;);}
+I main(I ac,S av[]){FT_Library l;FT_Init_FreeType(&l);
+  b(UK==(err=FT_New_Face(l,FONT,0,&f)),p("fmt err: %s\n",FONT),b(err,p("other err: %d",err),));
   // p("%ld\n",f->num_glyphs);
-  met(f);
-  I ppem=f->units_per_EM;UI n;
+  met(f);I ppem=f->units_per_EM;UI n;
   M(p("(COMMENT c: %d, wd: %g, ht: %g, dp: %d)\n",
       n,wd[n]inpp,ht[n]inpp,dp[n]););
-  p("distinct ht: %d\n",cd(ht));
-  p("distinct dp: %d\n",cd(dp));
+  p("distinct ht: %d\n",cd(ht));p("distinct dp: %d\n",cd(dp));
   FT_Done_FreeType(l);}
