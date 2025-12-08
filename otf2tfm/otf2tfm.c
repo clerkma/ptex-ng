38a30eb9649fe254bcdf8b312f78a70dd8a4bda5
LdBeth <andpuke@foxmail.com>
2025-12-08 17:09:33 +0800

    refine program

--- otf2tfm/otf2tfm.c
+++ otf2tfm/otf2tfm.c
@@ -4,8 +4,9 @@
 #include FT_FREETYPE_H
 #include FT_BBOX_H
 typedef int I;typedef unsigned int UI;typedef char* S;typedef FT_Face F;
-#define FONT "/Users/ldbeth/Library/Fonts/ACaslonPro-Regular.ttf"
+#define FONT "/Users/ldbeth/Library/Fonts/Bookerly-Regular.ttf"
 #define b(c,a,b) if(c){a;}else{b;}
+#define g f->glyph
 #define p printf
 #define R return
 #define UK FT_Err_Unknown_File_Format
@@ -14,15 +15,16 @@ typedef int I;typedef unsigned int UI;typedef char* S;typedef FT_Face F;
 #define FN(n) _ n(F f)
 #define inpp /(double)ppem
 F f;static FT_Error err;I wd[256];I ht[256];I dp[256];
+static I cd(I *a){I c=0;I n;
+  M(I j;for(j=n+1;j<256;j++)if(a[n]==a[j])break;if(j==256)c++;);
+  R c;}
 _ bbox(FT_Outline* o,int *h, int* d){FT_BBox b;
   FT_Outline_Get_BBox(o,&b);*h=b.yMax;*d=b.yMin;}
 FN(met){UI n;
-  // For(256,)
   M(err=FT_Load_Glyph(f,n,FT_LOAD_NO_SCALE);
     b(err,p("load glyph %d failed\n",n),);
-    wd[n]=f->glyph->advance.x;
-    bbox(&f->glyph->outline,&ht[n],&dp[n]);
-   );}
+    wd[n]=g->advance.x;
+    bbox(&g->outline,&ht[n],&dp[n]););}
 I main(I ac,S av[]){FT_Library l;
   FT_Init_FreeType(&l);
   err=FT_New_Face(l,FONT,0,&f);
@@ -30,5 +32,8 @@ I main(I ac,S av[]){FT_Library l;
   // p("%ld\n",f->num_glyphs);
   met(f);
   I ppem=f->units_per_EM;UI n;
-  M(p("c: %d, wd: %f, ht: %f\n",n,wd[n]inpp,ht[n]inpp););
-}
+  M(p("(COMMENT c: %d, wd: %g, ht: %g, dp: %d)\n",
+      n,wd[n]inpp,ht[n]inpp,dp[n]););
+  p("distinct ht: %d\n",cd(ht));
+  p("distinct dp: %d\n",cd(dp));
+  FT_Done_FreeType(l);}
