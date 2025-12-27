419c53e789a4bbc94b0a7a63b3927bf817e601f5
Clerk Ma <clerkma@gmail.com>
2025-12-27 20:50:02 +0800

    updated code (from eptex 251130).

--- src/aptex-src.c
+++ src/aptex-src.c
@@ -15485,8 +15485,17 @@ restart:
         }
         else if (cur_chr == last_node_font_code)
         {
-          cur_val_level = ident_val;
-          cur_val = null_font + font_id_base;
+          if (level != tok_val)
+          {
+            print_err("Missing number, treated as zero");
+            help3("A number should have been here; I inserted `0'.",
+              "(If you can't figure out why I needed to see a number,",
+              "look up `weird error' in the index to The TeXbook.)");
+            back_error();
+            scanned_result(0, dimen_val);
+          }
+          else
+            scanned_result(null_font + font_id_base, ident_val);
         }
         else
           cur_val_level = cur_chr;
