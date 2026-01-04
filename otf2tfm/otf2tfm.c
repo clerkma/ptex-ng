6a92196e101005dfe16774ad54f0bc2399da9edd
LdBeth <andpuke@foxmail.com>
2026-01-04 09:41:00 +0800

    use for loop to simplify

--- otf2tfm/otf2tfm.c
+++ otf2tfm/otf2tfm.c
@@ -14,7 +14,6 @@ typedef int I;typedef unsigned int UI;typedef short S;typedef const char*C;typed
 #define R return
 #define G(w) FT_##w##_Glyph
 #define _I static I
-#define UK FT_Err_Unknown_File_Format
 #define M(b...) for(n=0;n<256;++n){b;}
 #define FN(n) static void n(F f)
 static FT_Error err;I w[256];S h[256],d[256];
@@ -22,7 +21,8 @@ FN(met){UI n;M($(G(Load)(f,n,FT_LOAD_NO_SCALE),E("ld gyp %d failed\n",n),);
   w ng advance.x;FT_Set_Pixel_Sizes(f,10,0);G(Load)(f,n,0);
   G(Render)(g,FT_RENDER_MODE_LIGHT);h ng bitmap_top;d ng bitmap.rows-h[n];);}
 I main(I ac,C*av){FT_Library l;FT_Init_FreeType(&l);F f;$(ac<2,P("input\n");R 1,);
-  $(UK==(err=FT_New_Face(l,av[1],0,&f)),E("fmt err: %s\n",av[1]);R 3,
-    $(err,E("other err: %d\n",err);R 4,));P("num g: %ld\n",f->num_glyphs);met(f);UI n;
+  $(FT_Err_Unknown_File_Format==(err=FT_New_Face(l,av[1],0,&f)),
+    E("fmt err: %s\n",av[1]);R 3,$(err,E("other err: %d\n",err);R 4,));
+  P("num g: %ld\n",f->num_glyphs);met(f);UI n;
   M(P("(COMMENT c: %d, wd: %d, ht: %d, dp: %d)\n",n,w[n],h[n],d[n]););
   FT_Done_FreeType(l);}
