6bdf62231dcda755f77004576aa950bee6764329
Clerk Ma <1113706230@qq.com>
2026-05-26 09:59:58 +0800

    updated task/install-blade-v2.sh.

--- task/install-blade-v2.sh
+++ task/install-blade-v2.sh
@@ -3,11 +3,10 @@
 set -e
 
 if [[ -d blade-build ]]; then
-    git -C blade-build pull
-else
-    git clone https://github.com/blade-build/blade-build
+    rm -rfv blade-build
 fi
 
+git clone https://github.com/blade-build/blade-build
 cd blade-build
 git switch v2
 git checkout -f 3295898226ee43f01f3b9c7081d6f24d7f4556b7
