ed0a43c709b2ffa5a3f83c72a48e1159818e5f00
Clerk Ma <1113706230@qq.com>
2026-05-26 09:45:24 +0800

    blade: no pollution usage.

--- task/install-blade-v2.sh
+++ task/install-blade-v2.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-mkdir -p ~/.cache
-
 if [[ -d ~/.cache/blade-build ]]; then
-    git -C ~/.cache/blade-build pull
+    git -C blade-build pull
 else
-    git clone https://github.com/blade-build/blade-build ~/.cache/blade-build
+    git clone https://github.com/blade-build/blade-build
 fi
 
-cd ~/.cache/blade-build
+cd blade-build
 git switch v2
 git checkout 3295898226ee43f01f3b9c7081d6f24d7f4556b7
-./install
-
